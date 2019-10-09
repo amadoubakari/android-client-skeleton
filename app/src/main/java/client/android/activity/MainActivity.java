af87cb745720bde846b230ea47a54d14cc06d5be
AMADOU BAKARI <amadou_bakari@yahoo.fr>
2019-10-09 17:10:59 +0100

    Initialisation du projet

---
+++ app/src/main/java/client/android/activity/MainActivity.java
@@ -0,0 +1,64 @@
+package client.android.activity;
+
+import android.util.Log;
+import client.android.R;
+import client.android.architecture.core.AbstractActivity;
+import client.android.architecture.core.AbstractFragment;
+import client.android.architecture.custom.Session;
+import client.android.dao.service.Dao;
+import client.android.dao.service.IDao;
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+@EActivity
+@OptionsMenu(R.menu.menu_main)
+public class MainActivity extends AbstractActivity {
+
+  // couche [DAO]
+  @Bean(Dao.class)
+  protected IDao dao;
+  // session
+  private Session session;
+
+  // méthodes classe parent -----------------------
+  @Override
+  protected void onCreateActivity() {
+    // log
+    if (IS_DEBUG_ENABLED) {
+      Log.d(className, "onCreateActivity");
+    }
+    // session
+    this.session = (Session) super.session;
+    // todo : on continue les initialisations commencées par la classe parent
+  }
+
+  @Override
+  protected IDao getDao() {
+    return dao;
+  }
+
+  @Override
+  protected AbstractFragment[] getFragments() {
+    // todo : définir les fragments ici
+    return new AbstractFragment[0];
+  }
+
+
+  @Override
+  protected CharSequence getFragmentTitle(int position) {
+    // todo : définir les titres des fragments ici
+    return null;
+  }
+
+  @Override
+  protected void navigateOnTabSelected(int position) {
+    // todo : navigation par onglets - définir la vue à afficher lorsque l'onglet n° [position] est sélectionné
+  }
+
+  @Override
+  protected int getFirstView() {
+    // todo : définir le n° de la première vue (fragment) à afficher
+    return 0;
+  }
+}
---
+++ app/src/main/java/client/android/dao/service/IDao.java
@@ -0,0 +1,25 @@
+package client.android.dao.service;
+
+import rx.Observable;
+
+public interface IDao {
+  // Url du service web
+  void setUrlServiceWebJson(String url);
+
+  // utilisateur
+  void setUser(String user, String mdp);
+
+  // timeout du client
+  void setTimeout(int timeout);
+
+  // authentification basique
+  void setBasicAuthentification(boolean isBasicAuthentificationNeeded);
+
+  // mode debug
+  void setDebugMode(boolean isDebugEnabled);
+
+  // délai d'attente en millisecondes du client avant requête
+  void setDelay(int delay);
+
+  // todo : déclarez votre interface ici
+}
