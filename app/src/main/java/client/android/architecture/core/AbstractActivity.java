a6c17d5d9a65f0a665e1b3ffcd911a6bb83c263b
AMADOU BAKARI <amadou_bakari@yahoo.fr>
2019-10-11 11:16:57 +0100

    Refactoring: migration du design android vers androidX

--- app/src/main/java/client/android/architecture/core/AbstractActivity.java
+++ app/src/main/java/client/android/architecture/core/AbstractActivity.java
@@ -1,13 +1,13 @@
 package client.android.architecture.core;
 
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
--- app/src/main/java/client/android/architecture/core/AbstractFragment.java
+++ app/src/main/java/client/android/architecture/core/AbstractFragment.java
@@ -2,7 +2,7 @@ package client.android.architecture.core;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
