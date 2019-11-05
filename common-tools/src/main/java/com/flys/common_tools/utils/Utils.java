1df585b5781d3f93293750168d106350fd37f215
AMADOU BAKARI <amadou_bakari@yahoo.fr>
2019-11-05 13:58:22 +0100

    Feature: recommand to a friend

--- common-tools/src/main/java/com/flys/common_tools/utils/Utils.java
+++ common-tools/src/main/java/com/flys/common_tools/utils/Utils.java
@@ -1,13 +1,17 @@
 package com.flys.common_tools.utils;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Environment;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class Utils implements Serializable {
@@ -49,5 +53,55 @@ public class Utils implements Serializable {
         mi.setTitle(mNewTitle);
     }
 
+
+    //Elle permet de partager un texte avec d'autres applications(facebook, whatsapp, twitter, skype, etc. ), ce texte peut être une URL
+
+    /**
+     * @param context
+     * @param subject object d'envoie du fichier
+     * @param text    le texte à envoyer et ça peut être une URL
+     * @param title   le titre de la fenêtre qui va s'afficher
+     */
+    public static void shareText(Context context, String subject, String text, String title) {
+        Intent share = new Intent(android.content.Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT );
+
+        // Add data to the intent, the receiving app will decide
+        // what to do with it.
+        share.putExtra(Intent.EXTRA_SUBJECT, subject);
+        //share.putExtra(Intent.EXTRA_SUBJECT, "URL de la cagnotte");
+        share.putExtra(Intent.EXTRA_TEXT, text);
+        //share.putExtra(Intent.EXTRA_TEXT, "http://www.codeofaninja.com");
+
+        //context.startActivity(Intent.createChooser(share, "Partager ma cagnotte."));
+        context.startActivity(Intent.createChooser(share, title));
+    }
+
+    //Elle permet de partager un fichier
+
+    /**
+     *
+     * @param context
+     */
+    public static void shareFile(Context context) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // If you want to share a png image only, you can do:
+        // setType("image/png"); OR for jpeg: setType("image/jpeg");
+        share.setType("image/*");
+
+        // Make sure you put example png image named myImage.png in your
+        // directory
+        String imagePath = Environment.getExternalStorageDirectory()
+                + "/myImage.png";
+
+        File imageFileToShare = new File(imagePath);
+
+        Uri uri = Uri.fromFile(imageFileToShare);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+        context.startActivity(Intent.createChooser(share, "Share Image!"));
+    }
 
 }
